cc39d068a6ad2b67f3fe28e397ed27a9e87e4a1e
pchchv <ipchchv@gmail.com>
2025-10-27 20:36:26 +0300

    feat: add app runner

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'data.dart';
 
+void main() {
+  runApp(const DocumentApp());
+}
+
 class DocumentScreen extends StatelessWidget {
   final Document document;
   const DocumentScreen({required this.document, super.key});
