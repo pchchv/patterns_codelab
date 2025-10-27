0702e4d6bbc6c466d8e169f8de7b0acf3c311745
pchchv <ipchchv@gmail.com>
2025-10-27 21:09:44 +0300

    refactor: remove unused JSON

--- lib/data.dart
+++ lib/data.dart
@@ -1,33 +1,5 @@
-import 'dart:convert';
-
-const documentJson = '''
-{
-  "metadata": {
-    "title": "My Document",
-    "modified": "2025-05-10"
-  },
-  "blocks": [
-    {
-      "type": "h1",
-      "text": "Chapter 1"
-    },
-    {
-      "type": "p",
-      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    },
-    {
-      "type": "checkbox",
-      "checked": false,
-      "text": "Learn Dart 3"
-    }
-  ]
-}
-''';
-
 class Document {
-  final Map<String, Object?> _json;
-  Document() : _json = jsonDecode(documentJson);
-
+  Document();
   (String, {DateTime modified}) get metadata {
     const title = 'My Document';
     final now = DateTime.now();
