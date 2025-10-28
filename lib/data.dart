650f70060526105ba9bb420f9513a1247d6170ca
pchchv <ipchchv@gmail.com>
2025-10-28 19:32:37 +0300

    feat: implement a Block class

--- lib/data.dart
+++ lib/data.dart
@@ -38,3 +38,17 @@ class Document {
     }
   }
 }
+
+class Block {
+  final String type;
+  final String text;
+  Block(this.type, this.text);
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json case {'type': final type, 'text': final text}) {
+      return Block(type, text);
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
