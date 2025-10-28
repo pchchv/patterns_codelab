2692194e9c73a19194a4ba6bd15f86e139442461
pchchv <ipchchv@gmail.com>
2025-10-28 19:51:54 +0300

    feat(formatDate): add formatting logic for weeks

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ String formatDate(DateTime dateTime) {
     Duration(inDays: 0) => 'today',
     Duration(inDays: 1) => 'tomorrow',
     Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days) when days > 7 => '${days ~/ 7} weeks from now',
+    Duration(inDays: final days) when days < -7 =>
+      '${days.abs() ~/ 7} weeks ago',
     Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
     Duration(inDays: final days) => '$days days from now',
   };
