2762eb83ee4b27d0f39d0fcdc98ce22bfce0b193
pchchv <ipchchv@gmail.com>
2025-10-28 19:27:47 +0300

    refactor: implement a version of metadata that reads values ​​from _json

--- lib/data.dart
+++ lib/data.dart
@@ -1,9 +1,44 @@
-class Document {
-  Document();
-  (String, {DateTime modified}) get metadata {
-    const title = 'My Document';
-    final now = DateTime.now();
+import 'dart:convert';
 
-    return (title, modified: now);
+const documentJson = '''
+{
+  "metadata": {
+    "title": "My Document",
+    "modified": "2023-05-10"
+  },
+  "blocks": [
+    {
+      "type": "h1",
+      "text": "Chapter 1"
+    },
+    {
+      "type": "p",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      "type": "checkbox",
+      "checked": false,
+      "text": "Learn Dart 3"
+    }
+  ]
+}
+''';
+
+class Document {
+  final Map<String, Object?> _json;
+  Document() : _json = jsonDecode(documentJson);
+
+  (String, {DateTime modified}) get metadata {
+    if (_json.containsKey('metadata')) {
+      final metadataJson = _json['metadata'];
+      if (metadataJson is Map) {
+        final title = metadataJson['title'] as String;
+        final localModified = DateTime.parse(
+          metadataJson['modified'] as String,
+        );
+        return (title, modified: localModified);
+      }
+    }
+    throw const FormatException('Unexpected JSON');
   }
 }
